b16faf8169eea4d5c4662af709276a8b6d3a74ec
Todd Fincannon <todd@toddfincannon.com>
2017-07-17 21:08:20 -0400

    store sde log values with canonical names that serve as acceptable keys

--- src/sde-log.js
+++ src/sde-log.js
@@ -1,6 +1,7 @@
 const fs = require('fs-extra')
 const path = require('path')
 const R = require('ramda')
+const { canonicalName } = require('./Helpers')
 const F = require('./futil')
 
 let command = 'log [options] <logfile>'
@@ -24,19 +25,27 @@ let log = (logPathname, opts) => {
 }
 let exportDat = (logPathname, datPathname) => {
   let lines = fs.readFileSync(logPathname, 'utf8').split(/\r?\n/)
-  let varNames = []
+  let varNames = {}
+  let varKeys = []
   let steps = []
   for (let line of lines) {
     if (R.isEmpty(varNames)) {
-      varNames = R.map(v => v.toLowerCase(), line.split('\t'))
+      // Turn the var names in the header line into acceptable keys in canonical format.
+      let header = line.split('\t')
+      varKeys = R.map(v => canonicalName(v), header)
+      varNames = R.zipObj(varKeys, header)
     } else if (!R.isEmpty(line)) {
-      steps.push(R.zipObj(varNames, line.split('\t')))
+      // Add an object with values at this time step.
+      steps.push(R.zipObj(varKeys, line.split('\t')))
     }
   }
-  for (let varName of varNames) {
-    if (varName !== 'time') {
-      F.emitLine(varName)
-      for (let step of steps) F.emitLine(`${step.time}\t${step[varName]}`)
+  // Emit all time step values for each var name.
+  for (let varKey of varKeys) {
+    if (varKey !== '_time') {
+      F.emitLine(varNames[varKey])
+      for (let step of steps) {
+        F.emitLine(`${step['_time']}\t${step[varKey]}`)
+      }
     }
   }
   F.writeBuf(datPathname)
