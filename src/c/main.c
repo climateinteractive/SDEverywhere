b351d6a82de56864e8975e81c1bb89b075d319e5
Todd Fincannon <todd@toddfincannon.com>
2019-03-25 16:36:25 -0700

    output data without newlines or a header, for embedding reference data

--- src/c/main.c
+++ src/c/main.c
@@ -3,6 +3,8 @@
 int main(int argc, char** argv) {
   // TODO make the input buffer size dynamic
   char inputs[1000];
+  // When true, output data without newlines or a header, suitable for embedding reference data.
+  bool raw_output = false;
   // Try to read input from a file named in the argument.
   if (argc > 1) {
     FILE* instream = fopen(argv[1], "r");
@@ -13,29 +15,40 @@ int main(int argc, char** argv) {
         inputs[len-1] = '\0';
       }
     }
+    if (argc > 2) {
+      // Set option flags.
+      if (strcmp(argv[2], "--raw") == 0) {
+        raw_output = true;
+      }
+    }
   }
   else {
     *inputs = '\0';
   }
   // Run the model and get output for all time steps.
   char* outputs = run_model(inputs);
+  if (raw_output) {
+    // Write raw output data directly.
+    fputs(outputs, stdout);
+  } else {
   // Write a header for output data.
-  printf("%s\n", getHeader());
-  // Write tab-delimited output data, one line per output time step.
-  if (outputs != NULL) {
-    char* p = outputs;
-    while (*p) {
-      char* line = p;
-      for (size_t i = 0; i < numOutputs; i++) {
-        if (i > 0) {
-          p++;
+    printf("%s\n", getHeader());
+    // Write tab-delimited output data, one line per output time step.
+    if (outputs != NULL) {
+      char* p = outputs;
+      while (*p) {
+        char* line = p;
+        for (size_t i = 0; i < numOutputs; i++) {
+          if (i > 0) {
+            p++;
+          }
+          while (*p && *p != '\t') {
+            p++;
+          }
         }
-        while (*p && *p != '\t') {
-          p++;
-        }
+        *p++ = '\0';
+        printf("%s\n", line);
       }
-      *p++ = '\0';
-      printf("%s\n", line);
     }
   }
   finish();
