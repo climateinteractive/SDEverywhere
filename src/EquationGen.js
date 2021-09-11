1ea5ed1e68793c6f29615f8275207e339ac743b2
Todd Fincannon <todd@toddfincannon.com>
2021-09-10 21:10:10 -0700

    run prettier on source files

--- src/EquationGen.js
+++ src/EquationGen.js
@@ -767,7 +767,9 @@ export default class EquationGen extends ModelReader {
         exprs[i].accept(this)
         // For DELAY FIXED, also initialize the support struct out of band, as it is not a Vensim var.
         if (fn === '_DELAY_FIXED') {
-          this.emit(`;\n  ${this.var.fixedDelayVarName}${this.lhsSubscriptGen(this.var.subscripts)} = __new_fixed_delay(`)
+          this.emit(
+            `;\n  ${this.var.fixedDelayVarName}${this.lhsSubscriptGen(this.var.subscripts)} = __new_fixed_delay(`
+          )
           this.setArgIndex(1)
           exprs[1].accept(this)
           this.emit(', ')
