590f8ed088b747d7ef9a3fa79ad579304fa19120
Chris Campbell <ccampbell@climateinteractive.org>
2021-10-23 00:11:58 -0700

    fix: wrap conditional branch expression in parentheses when optimizing IF THEN ELSE

--- src/EquationGen.js
+++ src/EquationGen.js
@@ -874,6 +874,7 @@ export default class EquationGen extends ModelReader {
       const condText = ctx.expr(0).getText()
       const condValue = Model.getConstantExprValue(condText)
       if (condValue !== undefined) {
+        this.emit('(')
         if (condValue !== 0) {
           // Emit only the "if true" branch
           this.setArgIndex(1)
@@ -883,6 +884,7 @@ export default class EquationGen extends ModelReader {
           this.setArgIndex(2)
           ctx.expr(2).accept(this)
         }
+        this.emit(')')
       } else {
         // Emit a normal if/else with both branches
         this.emit(fn)
