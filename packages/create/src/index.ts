e0f6a1b8fb986888e21b71127cb53a88ec341ee7
Chris Campbell <ccampbell@climateinteractive.org>
2022-09-20 21:43:36 -0700

    fix: allow user to choose mdl if there are multiple

--- packages/create/src/index.ts
+++ packages/create/src/index.ts
@@ -1,6 +1,7 @@
 // Copyright (c) 2022 Climate Interactive / New Venture Fund
 
 import { existsSync, mkdtempSync, readdirSync } from 'fs'
+import { readdir } from 'fs/promises'
 import { copy } from 'fs-extra'
 import { tmpdir } from 'os'
 import { relative, join as joinPath, resolve as resolvePath } from 'path'
@@ -10,6 +11,7 @@ import { execa, execaCommand } from 'execa'
 import { bgCyan, black, bold, cyan, dim, green, red, reset, yellow } from 'kleur/colors'
 import type { Ora } from 'ora'
 import ora from 'ora'
+import type { Choice } from 'prompts'
 import prompts from 'prompts'
 import detectPackageManager from 'which-pm-runs'
 import yargs from 'yargs-parser'
@@ -46,19 +48,17 @@ async function chooseProjectDir(): Promise<string> {
   }
 
   const showValidDirMsg = (dir: string) => {
-    const ack = ora({
+    ora({
       color: 'green',
-      text: `Using "${bold(dir)}" as project directory.`
-    })
-    ack.succeed()
+      text: green(`Using "${bold(dir)}" as the project directory.`)
+    }).succeed()
   }
 
   const showInvalidDirMsg = (dir: string) => {
-    const reject = ora({
+    ora({
       color: 'red',
       text: `"${bold(dir)}" contains existing 'package.json' and/or 'packages' directory, stopping.`
-    })
-    reject.fail()
+    }).fail()
   }
 
   // See if project directory is provided on command line
@@ -244,6 +244,69 @@ async function chooseTemplate(projDir: string): Promise<void> {
   templateSpinner.succeed()
 }
 
+async function chooseMdlFile(projDir: string): Promise<string> {
+  // Find all `.mdl` files in the project directory
+  // From https://stackoverflow.com/a/45130990
+  async function getFiles(dir: string): Promise<string[]> {
+    const dirents = await readdir(dir, { withFileTypes: true })
+    const files = await Promise.all(
+      dirents.map(dirent => {
+        const res = resolvePath(dir, dirent.name)
+        return dirent.isDirectory() ? getFiles(res) : res
+      })
+    )
+    return files.flat()
+  }
+  const allFiles = await getFiles(projDir)
+  const mdlFiles = allFiles.filter(f => f.endsWith('.mdl')).map(f => relative(projDir, f))
+  const mdlChoices = mdlFiles.map(f => {
+    return {
+      title: f,
+      value: f
+    } as Choice
+  })
+
+  let mdlFile: string
+  if (mdlFiles.length === 0) {
+    // No mdl files found; print error message and exit
+    // TODO: Offer to add a basic mdl to get the user started
+    ora({
+      color: 'red',
+      text: `No mdl files were found in "${projDir}". Add your mdl file to that directory and try again.`
+    }).fail()
+    process.exit(1)
+  } else if (mdlFiles.length === 1) {
+    // Only one mdl file
+    ora().succeed(`Found "${mdlFiles[0]}", will configure the project to use that mdl file.`)
+    mdlFile = mdlFiles[0]
+  } else {
+    // Multiple mdl files found; allow the user to choose one
+    // TODO: Eventually we should allow the user to choose to flatten if there are multiple submodels
+    const options = await prompts(
+      [
+        {
+          type: 'select',
+          name: 'mdlFile',
+          message: 'It looks like there are multiple mdl files. Which one would you like to use?',
+          choices: mdlChoices
+        }
+      ],
+      { onCancel: () => ora().info(dim('Operation cancelled.')) }
+    )
+    if (!options.mdlFile) {
+      process.exit(1)
+    }
+    mdlFile = options.mdlFile
+  }
+
+  ora({
+    color: 'green',
+    text: green(`Using "${bold(mdlFile)}" as the model for the project.`)
+  }).succeed()
+
+  return mdlFile
+}
+
 async function chooseInstall(projDir: string): Promise<void> {
   // Prompt the user
   const installResponse = await prompts(
@@ -321,15 +384,20 @@ export async function main(): Promise<void> {
   // Prompt the user to select a template
   await chooseTemplate(projDir)
 
+  // Prompt the user to select an mdl file
+  const mdlPath = await chooseMdlFile(projDir)
+  // TODO
+  console.log(mdlPath)
+
+  // TODO: Prompt the user to choose input/output vars (if default template chosen)
+
+  // TODO: Prompt the user to install Emscripten SDK
+
   // Prompt the user to install dependencies
   await chooseInstall(projDir)
 
   // Prompt the user to initialize a git repo
   await chooseGitInit(projDir)
-
-  // TODO: See if project contains an mdl file, if not, use hello-world
-  // TODO: Set up sde.config.js file to use selected mdl file
-  // TODO: Fill in default values for config files
 
   ora({ text: green('Setup complete!') }).succeed()
 
