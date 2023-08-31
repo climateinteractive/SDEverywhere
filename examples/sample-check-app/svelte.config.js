19edfdba7f8ebb17847141edf702b0c5a8366d07
Chris Campbell <ccampbell@climateinteractive.org>
2023-08-30 22:27:24 -0700

    build: ignore a couple svelte-check warnings for the time being

--- examples/sample-check-app/svelte.config.js
+++ examples/sample-check-app/svelte.config.js
@@ -1,5 +1,14 @@
 import sveltePreprocess from 'svelte-preprocess'
 
 export default {
-  preprocess: sveltePreprocess({})
+  preprocess: sveltePreprocess({}),
+  onwarn: (warning, defaultHandler) => {
+    // TODO: We should resolve these warnings instead of ignoring them
+    if (warning.code === 'a11y-click-events-have-key-events') {
+      return
+    }
+
+    // Handle all other warnings normally
+    defaultHandler(warning)
+  }
 }
