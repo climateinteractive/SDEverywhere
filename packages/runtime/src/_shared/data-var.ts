71d1666d18ad032fca519f8e099def3d347a02ac
Chris Campbell <ccampbell@climateinteractive.org>
2024-05-02 10:15:43 -0700

    fix: drop DataVar (not needed for this branch)

--- packages/runtime/src/_shared/data-var.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024 Climate Interactive / New Venture Fund
-
-import type { Point } from './outputs'
-import type { VarSpec } from './var-indices'
-
-export interface DataVar {
-  varSpec: VarSpec
-  points: Point[]
-}
