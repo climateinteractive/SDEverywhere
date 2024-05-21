5268af5a1292813c12437796a25e0509f049bd61
Chris Campbell <ccampbell@climateinteractive.org>
2024-05-20 17:17:52 -0700

    refactor: move model-listing files to a separate directory

--- packages/runtime/src/model-runner/index.ts
+++ packages/runtime/src/model-runner/index.ts
@@ -2,6 +2,3 @@
 
 export * from './model-runner'
 export * from './synchronous-model-runner'
-
-/** @hidden This is not part of the public API; exposed only for use in testing tools. */
-export * from './model-listing'
