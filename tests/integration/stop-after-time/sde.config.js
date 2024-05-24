3f820a6ad71e4e4564740ca824fa865312146bd3
Chris Campbell <ccampbell@climateinteractive.org>
2024-05-24 13:35:09 -0700

    test: add integration test for stopAfterTime option (not yet implemented for Wasm case)

---
+++ tests/integration/stop-after-time/sde.config.js
@@ -0,0 +1,28 @@
+import { wasmPlugin } from '@sdeverywhere/plugin-wasm'
+import { workerPlugin } from '@sdeverywhere/plugin-worker'
+
+const genFormat = process.env.GEN_FORMAT === 'c' ? 'c' : 'js'
+
+export async function config() {
+  return {
+    genFormat,
+    modelFiles: ['stop-after-time.mdl'],
+
+    modelSpec: async () => {
+      return {
+        inputs: [{ varName: 'Y', defaultValue: 0, minValue: -10, maxValue: 10 }],
+        outputs: [{ varName: 'Z' }],
+        datFiles: []
+      }
+    },
+
+    plugins: [
+      // If targeting WebAssembly, generate a `generated-model.js` file
+      // containing the Wasm model
+      genFormat === 'c' && wasmPlugin(),
+
+      // Generate a `worker.js` file that runs the generated model in a worker
+      workerPlugin()
+    ]
+  }
+}
