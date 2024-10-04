5f34d717f89f2a7289adeac547ca6b4b887d5b6c
Chris Campbell <ccampbell@climateinteractive.org>
2024-10-03 17:12:40 -0700

    fix: correct sample-check-bundle so that define works correctly with latest vite

---
+++ examples/sample-check-bundle/src/env.d.ts
@@ -0,0 +1,6 @@
+// Copyright (c) 2024 Climate Interactive / New Venture Fund
+
+// Declare constants that are injected at build time
+declare const __MODEL_SIZE_IN_BYTES__: number
+declare const __DATA_SIZE_IN_BYTES__: number
+declare const __MODEL_VERSION__: number
