c52dec3c732b652b8fdff6563650a9bcd973da09
Chris Campbell <ccampbell@climateinteractive.org>
2025-03-07 18:35:48 -0800

    fix: update sample-check-tests to demonstrate summary customization options

--- examples/sample-check-tests/src/index.ts
+++ examples/sample-check-tests/src/index.ts
@@ -2,6 +2,10 @@
 
 import type {
   Bundle,
+  ComparisonGroupSummariesByCategory,
+  ComparisonGroupSummary,
+  ComparisonReportOptions,
+  ComparisonReportSummarySection,
   ComparisonSpecs,
   ComparisonSpecsSource,
   ConfigInitOptions,
@@ -24,6 +28,9 @@ const comparisonsYaml: ComparisonSpecsSource[] = Object.entries(comparisonsYamlG
 })
 
 export function getConfigOptions(bundleL: Bundle, bundleR: Bundle, opts?: ConfigInitOptions): ConfigOptions {
+  const nameL = 'Sample Model Current'
+  const nameR = 'Sample Model Baseline'
+
   // Configure the set of input scenarios used for comparisons; this includes
   // the default matrix of scenarios
   const baseComparisonSpecs = createBaseComparisonSpecs(bundleL, bundleR)
@@ -40,9 +47,22 @@ export function getConfigOptions(bundleL: Bundle, bundleR: Bundle, opts?: Config
   // (see `getOutputVars` in `sample-model-bundle`)
   const renamedDatasetKeys: Map<DatasetKey, DatasetKey> = new Map([['Model__output_w_v1', 'Model__output_w_v2']])
 
+  // Customize the report sections if the kind is 'custom'
+  const reportKind: string = 'custom'
+  // const reportKind: string = 'default'
+  let reportOptions: ComparisonReportOptions
+  if (reportKind === 'custom') {
+    reportOptions = {
+      summarySectionsForComparisonsByScenario: summaries => {
+        return summarySectionsForComparisonsByScenario(summaries, nameL, nameR)
+      },
+      summarySectionsForComparisonsByDataset
+    }
+  }
+
   return {
     current: {
-      name: 'Sample Model Current',
+      name: nameL,
       bundle: bundleR
     },
     check: {
@@ -50,7 +70,7 @@ export function getConfigOptions(bundleL: Bundle, bundleR: Bundle, opts?: Config
     },
     comparison: {
       baseline: {
-        name: 'Sample Model Baseline',
+        name: nameR,
         bundle: bundleL
       },
       thresholds: [1, 5, 10],
@@ -70,7 +90,122 @@ export function getConfigOptions(bundleL: Bundle, bundleR: Bundle, opts?: Config
           }
           return []
         }
-      }
+      },
+      report: reportOptions
     }
   }
+}
+
+function summarySectionsForComparisonsByScenario(
+  summaries: ComparisonGroupSummariesByCategory,
+  nameL: string,
+  nameR: string
+): ComparisonReportSummarySection[] {
+  const allSummaries = Array.from(summaries.allGroupSummaries.values())
+
+  const sections: ComparisonReportSummarySection[] = []
+  function addSection(
+    headerText: string,
+    summaries: ComparisonGroupSummary[] | undefined,
+    include?: (id?: string) => boolean
+  ) {
+    if (summaries === undefined) {
+      return
+    }
+    if (include) {
+      summaries = summaries.filter(summary => {
+        if (summary.root.kind === 'scenario') {
+          const id = summary.root.id
+          if (id) {
+            return include(id)
+          }
+        }
+        return false
+      })
+    }
+    sections.push({
+      headerText,
+      summaries
+    })
+  }
+
+  // Add a section with a couple key scenarios (this demonstrates that we can highlight
+  // specific scenarios at top, and these scenarios can also appear in other sections)
+  addSection('Key scenarios', allSummaries, id => {
+    return id === 'baseline' || id === 'extreme_main_sliders_at_best_case'
+  })
+
+  // Add sections for scenarios with issues
+  addSection('Scenarios with errors', summaries.withErrors)
+  addSection(`Scenarios only valid in ${datasetSpan(nameL, 'left')}`, summaries.onlyInLeft)
+  addSection(`Scenarios only valid in ${datasetSpan(nameR, 'right')}`, summaries.onlyInRight)
+
+  // Add a section with all scenarios that produce differences
+  addSection('Scenarios producing differences', summaries.withDiffs)
+
+  // Add a section with all scenarios that don't produce differences
+  addSection('Scenarios NOT producing differences', summaries.withoutDiffs)
+
+  return sections
+}
+
+function summarySectionsForComparisonsByDataset(
+  summaries: ComparisonGroupSummariesByCategory
+): ComparisonReportSummarySection[] {
+  const allSummaries = Array.from(summaries.allGroupSummaries.values())
+
+  const sections: ComparisonReportSummarySection[] = []
+  function addSection(
+    headerText: string,
+    summaries: ComparisonGroupSummary[] | undefined,
+    include?: (key: string) => boolean
+  ) {
+    if (summaries === undefined) {
+      return
+    }
+    if (include) {
+      summaries = summaries.filter(summary => include(summary.root.key))
+    }
+    sections.push({
+      headerText,
+      summaries
+    })
+  }
+
+  // Add a section with a couple key outputs (this demonstrates that we can highlight
+  // specific datasets at top, and these datasets can also appear in other sections)
+  addSection('Key model outputs', allSummaries, key => {
+    return key === 'Model__output_x' || key === 'Model__output_y'
+  })
+
+  // Add sections for datasets with errors and removed/added datasets
+  addSection('Datasets with errors', summaries.withErrors)
+  addSection('Removed datasets', summaries.onlyInLeft)
+  addSection('Added datasets', summaries.onlyInRight)
+
+  // Add a section with all model outputs that have differences
+  addSection('Model outputs with differences', summaries.withDiffs, key => key.startsWith('Model__'))
+
+  // Add a section with all model outputs that have no differences
+  addSection('Model outputs without differences', summaries.withoutDiffs, key => key.startsWith('Model__'))
+
+  // Add a section with all other non-output datasets (this demonstrates that we
+  // can customize the sorting of datasets)
+  const staticWithDiffs = summaries.withDiffs.filter(summary => !summary.root.key.startsWith('Model__'))
+  const staticWithoutDiffs = summaries.withoutDiffs.filter(summary => !summary.root.key.startsWith('Model__'))
+  addSection('All static datasets', [
+    ...staticWithDiffs,
+    ...staticWithoutDiffs.sort((a, b) => a.root.key.localeCompare(b.root.key))
+  ])
+
+  return sections
+}
+
+/**
+ * Return an string containing an HTML `<span>` element with the appropriate
+ * color for the given bundle side (left or right).
+ */
+export function datasetSpan(name: string, side: 'left' | 'right'): string {
+  const color = side === 'left' ? 0 : 1
+  return `<span class="dataset-color-${color}">${name}</span>`
 }
