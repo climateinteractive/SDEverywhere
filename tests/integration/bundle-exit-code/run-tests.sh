6623c5bd45e68143e6cac988505ab5fa9d786c68
Chris Campbell <ccampbell@climateinteractive.org>
2025-04-10 18:12:21 -0700

    fix: correct the exit code reported by the `sde bundle` command (#618)
    Fixes #617

---
+++ tests/integration/bundle-exit-code/run-tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+# This script verifies that the exit code of the `sde bundle` command is 2 when the
+# `plugin-check` plugin reports that one or more check tests failed.
+#
+
+set +e # don't fail on error
+
+pnpm build-js
+exit_code=$?
+
+echo
+if [ $exit_code -ne 2 ]; then
+  echo "Test failed: 'sde bundle' reported exit code $exit_code but the expected exit code is 2"
+  echo
+  exit 1
+else
+  echo "Test passed: 'sde bundle' reported exit code 2 as expected"
+  echo
+fi
