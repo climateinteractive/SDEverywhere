85b9f46a8b5a3b39056f85ab1c6d32074aab40e6
Chris Campbell <ccampbell@climateinteractive.org>
2025-04-24 15:58:03 -0700

    fix: add different layout options

--- examples/template-svelte/packages/app/src/app.svelte
+++ examples/template-svelte/packages/app/src/app.svelte
@@ -7,20 +7,39 @@ import './global.css'
 import type { AppViewModel } from './app-vm'
 
 import SelectableGraph from './components/graph/selectable-graph.svelte'
+import Selector from './components/selector/selector.svelte'
 import Slider from './components/slider/slider.svelte'
 
 export let viewModel: AppViewModel
 const scenarios = viewModel.scenarios
+
+type Layout = 'layout_1_1' | 'layout_1_2' | 'layout_2_2'
+let selectedLayout: Layout = 'layout_1_2'
+const layoutOptions = [
+  { value: 'layout_1_1', stringKey: '1' },
+  { value: 'layout_1_2', stringKey: '2' },
+  { value: 'layout_2_2', stringKey: '4' }
+]
 </script>
 
 <!-- TEMPLATE -->
 <div class="app-container">
   <div class="top-container">
-    {#each viewModel.graphContainers as graphContainer}
-      <div class="selectable-graph-container">
-        <SelectableGraph viewModel={graphContainer} />
-      </div>
-    {/each}
+    <div class="options-container">
+      <div class="layout-label">Visible Graphs:</div>
+      <Selector
+        options={layoutOptions}
+        bind:value={selectedLayout}
+        onSelect={() => {}}
+      />
+    </div>
+    <div class="graphs-container {selectedLayout}">
+      {#each viewModel.graphContainers as graphContainer}
+        <div class="selectable-graph-container">
+          <SelectableGraph viewModel={graphContainer} />
+        </div>
+      {/each}
+    </div>
   </div>
   <div class="bottom-container">
     {#each $scenarios as scenario}
@@ -52,10 +71,30 @@ const scenarios = viewModel.scenarios
   flex-direction: column
   gap: 10px
 
+.options-container
+  display: flex
+  flex-direction: row
+  align-items: baseline
+  align-self: flex-end
+  margin-bottom: 10px
+  gap: 10px
+
+.graphs-container
+  display: grid
+  gap: 10px
+  width: 100%
+  overflow: hidden
+  &.layout_1_1
+    grid-template: 1fr
+  &.layout_1_2
+    grid-template: 1fr 1fr
+  &.layout_2_2
+    grid-template: 1fr 1fr / 1fr 1fr
+
 .selectable-graph-container
   display: flex
   box-sizing: border-box
-  width: 400px
+  width: 100%
   height: 300px
   padding: 10px
   border-radius: 10px
