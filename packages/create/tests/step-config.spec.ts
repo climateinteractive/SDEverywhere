c10b7acceec54e6c1335d8eaf56af12d11366325
Chris Campbell <ccampbell@climateinteractive.org>
2025-04-27 16:33:33 -0700

    test: remove workaround in create package test
    Tracking issue #633

--- packages/create/tests/step-config.spec.ts
+++ packages/create/tests/step-config.spec.ts
@@ -92,8 +92,7 @@ SAVEPER = TIME STEP ~~|
     writeFileSync(resolvePath(scratchDir, 'sample.mdl'), sampleMdlContent)
 
     // Run the create command
-    // TODO: Remove the --commit flag once the updated template is on main
-    const { stdin, stdout } = runCreate(['--commit', '17e5ac6', dirs.scratch])
+    const { stdin, stdout } = runCreate([dirs.scratch])
 
     // Wait for the template prompt
     await respondAndWaitForPrompt(stdin!, stdout!, undefined, promptMessages.template)
