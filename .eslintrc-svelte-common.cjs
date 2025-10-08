dedae01520585e18eb5aab9e2ec62bac2e3b9227
Chris Campbell <ccampbell@climateinteractive.org>
2025-10-08 12:42:09 -0700

    build: upgrade to eslint 9.x + update config files (#663)
    Fixes #657

--- .eslintrc-svelte-common.cjs
+++
@@ -1,4 +0,0 @@
-// TODO: This will be removed when we migrate to ESLint 9.x
-module.exports = {
-  extends: ['./.eslintrc-ts-common.cjs']
-}
--- .eslintrc.cjs
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  extends: ['./.eslintrc-ts-common.cjs']
-}
