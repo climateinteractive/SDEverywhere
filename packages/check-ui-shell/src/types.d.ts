e47cd779870649f0548ea3039b6bab64f2fd1d12
Chris Campbell <ccampbell@climateinteractive.org>
2025-10-09 14:34:44 -0700

    build: update comment

--- packages/check-ui-shell/src/types.d.ts
+++ packages/check-ui-shell/src/types.d.ts
@@ -8,7 +8,9 @@ declare namespace svelteHTML {
   }
 }
 
-// XXX: Workaround for issue where svelte-check fails to resolve the `yaml` package
+// XXX: Workaround for issue where svelte-check fails to resolve the `yaml` package types
 // referenced by the `check-core` package because we use `"moduleResolution": "bundler"`,
-// which causes...
+// which causes the `yaml/browser` directory to be picked up and that one doesn't include
+// a `.d.ts` file.  Using `declare module` is enough to silence the error and should have
+// no impact on normal builds.
 declare module 'yaml'
