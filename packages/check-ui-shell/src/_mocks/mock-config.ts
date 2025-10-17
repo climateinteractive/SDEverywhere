d7f50c79a71f8acae46f1a1c5e6a6699b08e084f
Chris Campbell <ccampbell@climateinteractive.org>
2025-10-17 11:26:04 -0700

    fix: add option for disabling comparisons in mock config

--- packages/check-ui-shell/src/_mocks/mock-config.ts
+++ packages/check-ui-shell/src/_mocks/mock-config.ts
@@ -1,6 +1,8 @@
 // Copyright (c) 2025 Climate Interactive / New Venture Fund
 
 import type {
+  ComparisonGroupSummary,
+  ComparisonOptions,
   ComparisonReportOptions,
   ComparisonReportSummarySection,
   ComparisonSpecs,
@@ -17,7 +19,7 @@ import {
 export function mockConfigOptions(
   bundleL: NamedBundle,
   bundleR: NamedBundle,
-  options?: { groupScenarios?: boolean }
+  options?: { comparisonsEnabled?: boolean; groupScenarios?: boolean }
 ): ConfigOptions {
   const allPositive = `
 - describe: Output 1
@@ -34,49 +36,61 @@ export function mockConfigOptions(
 
   const checkTests = [allPositive]
 
-  const comparisonSpecs: ComparisonSpecs = {
-    scenarios: [
-      scenarioWithAllInputsAtDefaultSpec(),
-      scenarioWithInputsSpec([inputAtPositionSpec('Constant 1', 'min')], { title: 'Constant 1', subtitle: 'at min' }),
-      scenarioWithInputsSpec([inputAtPositionSpec('Constant 1', 'max')], { title: 'Constant 1', subtitle: 'at max' })
-    ]
-  }
+  let comparisonOptions: ComparisonOptions | undefined
+  if (options?.comparisonsEnabled !== false) {
+    const comparisonSpecs: ComparisonSpecs = {
+      scenarios: [
+        scenarioWithAllInputsAtDefaultSpec(),
+        scenarioWithInputsSpec([inputAtPositionSpec('Constant 1', 'min')], { title: 'Constant 1', subtitle: 'at min' }),
+        scenarioWithInputsSpec([inputAtPositionSpec('Constant 1', 'max')], { title: 'Constant 1', subtitle: 'at max' })
+      ]
+    }
 
-  let reportOptions: ComparisonReportOptions
-  if (options?.groupScenarios === true) {
-    reportOptions = {
-      summarySectionsForComparisonsByScenario: summaries => {
-        const allSummaries = Array.from(summaries.allGroupSummaries.values())
-        const sections: ComparisonReportSummarySection[] = []
+    let reportOptions: ComparisonReportOptions
+    if (options?.groupScenarios === true) {
+      reportOptions = {
+        summarySectionsForComparisonsByScenario: summaries => {
+          const allSummaries = Array.from(summaries.allGroupSummaries.values())
+          const sections: ComparisonReportSummarySection[] = []
 
-        // Put the "All inputs at default" scenario in a "Key scenarios" section
-        const keyScenarios = allSummaries.filter(summary => summary.root.key.includes('all_inputs'))
-        sections.push({
-          headerText: 'Key scenarios',
-          rows: keyScenarios.map(summary => {
-            return {
-              groupSummary: summary
-            }
-          }),
-          // Set the stable flag to true so that this section appears in the filter panel
-          stable: true
-        })
+          // Put the "All inputs at default" scenario in a "Key scenarios" section
+          const keyScenarios: ComparisonGroupSummary[] = []
+          const baseline = allSummaries.shift()
+          keyScenarios.push(baseline)
+          sections.push({
+            headerText: 'Key scenarios',
+            rows: keyScenarios.map(summary => {
+              return {
+                groupSummary: summary
+              }
+            }),
+            // Set the stable flag to true so that this section appears in the filter panel
+            stable: true
+          })
 
-        // Put all other scenarios in an "Other scenarios" section
-        const otherScenarios = allSummaries.filter(summary => !summary.root.key.includes('all_inputs'))
-        sections.push({
-          headerText: 'Other scenarios',
-          rows: otherScenarios.map(summary => {
-            return {
-              groupSummary: summary
-            }
-          }),
-          // Set the stable flag to false so that the items in this section go into an "Other scenarios"
-          // group in the filter panel
-          stable: false
-        })
-        return sections
+          // Put all other scenarios in an "Other scenarios" section
+          const otherScenarios = allSummaries
+          sections.push({
+            headerText: 'Other scenarios',
+            rows: otherScenarios.map(summary => {
+              return {
+                groupSummary: summary
+              }
+            }),
+            // Set the stable flag to false so that the items in this section go into an "Other scenarios"
+            // group in the filter panel
+            stable: false
+          })
+          return sections
+        }
       }
+    }
+
+    comparisonOptions = {
+      baseline: bundleL,
+      thresholds: [1, 5, 10],
+      specs: [comparisonSpecs],
+      report: reportOptions
     }
   }
 
@@ -85,11 +99,6 @@ export function mockConfigOptions(
     check: {
       tests: checkTests
     },
-    comparison: {
-      baseline: bundleL,
-      thresholds: [1, 5, 10],
-      specs: [comparisonSpecs],
-      report: reportOptions
-    }
+    comparison: comparisonOptions
   }
 }
