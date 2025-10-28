fdf13ce27cd1219efc04e7fbd20ded985dcc8751
Chris Campbell <ccampbell@climateinteractive.org>
2025-10-28 12:19:16 -0700

    fix: add type with new sort mode options

--- packages/check-core/src/index.ts
+++ packages/check-core/src/index.ts
@@ -129,7 +129,10 @@ export { comparisonSummaryFromReport } from './comparison/report/comparison-repo
 
 export * from './comparison/report/comparison-group-types'
 export * from './comparison/report/comparison-group-scores'
+
 export { categorizeComparisonTestSummaries } from './comparison/report/comparison-grouping'
+
+export type { ComparisonSortMode } from './comparison/report/comparison-sort-mode'
 
 //
 // config
