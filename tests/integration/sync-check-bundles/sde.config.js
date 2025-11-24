834cf75a326b8f45639f69437374531bf11a840a
Chris Campbell <ccampbell@climateinteractive.org>
2025-11-24 12:23:38 -0800

    wip: add integration test for model-check bundle syncing

---
+++ tests/integration/sync-check-bundles/sde.config.js
@@ -0,0 +1,27 @@
+import { checkPlugin } from '@sdeverywhere/plugin-check'
+import { workerPlugin } from '@sdeverywhere/plugin-worker'
+
+export async function config() {
+  return {
+    modelFiles: ['sync-check-bundles.mdl'],
+
+    modelSpec: async () => {
+      return {
+        inputs: [
+          { varName: 'Production slope', defaultValue: 1, minValue: 1, maxValue: 10 },
+          { varName: 'Production start year', defaultValue: 2020, minValue: 2020, maxValue: 2070 },
+          { varName: 'Production years', defaultValue: 10, minValue: 0, maxValue: 30 }
+        ],
+        outputs: [{ varName: 'Total inventory' }]
+      }
+    },
+
+    plugins: [
+      // Generate a `worker.js` file that runs the generated model in a worker
+      workerPlugin(),
+
+      // Run model check
+      checkPlugin()
+    ]
+  }
+}
