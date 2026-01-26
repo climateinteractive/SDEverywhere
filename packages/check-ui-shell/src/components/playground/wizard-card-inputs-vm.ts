90278e15ac9936a9795434c02034fdbffb5ea4c7
Chris Campbell <ccampbell@climateinteractive.org>
2026-01-26 11:48:55 -0800

    fix: remove all code related to the old/unfinished wizard interface for creating new check tests

--- packages/check-ui-shell/src/components/playground/wizard-card-inputs-vm.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2022 Climate Interactive / New Venture Fund
-
-import type { Writable } from 'svelte/store'
-
-export interface WizardCardInputsViewModel {
-  todo: Writable<string>
-}
